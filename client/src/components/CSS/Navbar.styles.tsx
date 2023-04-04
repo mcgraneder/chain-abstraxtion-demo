cd187e3bffb5f5cddc86889b5660dc9e00a68c15
mcgraneder <mcgrane480@gmail.com>
2023-04-04 15:49:27 +0100

    change git repository location

---
+++ client/src/components/CSS/Navbar.styles.tsx
@@ -0,0 +1,75 @@
+import styled, { css } from 'styled-components';
+import tailwindConfig  from ".././../../tailwind.config";
+
+//create colours instead of hardcoding
+const colours = tailwindConfig.theme.extend.colors
+
+type GenericWrapper = {
+  isNavbarDark?: boolean;
+};
+
+type GenericBoxItemContainer = {
+    allignment?: "flex-start" | "flex-end"
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+export const Wrapper = styled.div<GenericWrapper>`
+  display: flex;
+  flex-flow: row nowrap;
+  width: 100%;
+  justify-content: space-between;
+  position: fixed;
+  top: 0px;
+  z-index: 100;
+  /* background: rgb(12, 18, 43); */
+
+  ${(isNavbarDark) =>
+    isNavbarDark &&
+    css`
+      background: rgb(12, 18, 43);
+    `}
+`;
+
+export const Nav = styled.nav`
+  padding: 20px 12px;
+  width: 100%;
+  height: 72px;
+  z-index: 2;
+  box-sizing: border-box;
+  display: block;
+`;
+
+export const Box = styled.div`
+  box-sizing: border-box;
+  vertical-align: initial;
+  -webkit-tap-highlight-color: transparent;
+  display: flex;
+  flex-wrap: nowrap;
+  height: 100%;
+`;
+
+
+export const BoxItemContainer = styled.div<GenericBoxItemContainer>`
+  box-sizing: border-box;
+  vertical-align: initial;
+  -webkit-tap-highlight-color: transparent;
+  justify-content: ${({ allignment }) => allignment};
+  display: flex;
+  flex-grow: 1;
+  flex-shrink: 1;
+  flex-basis: 0;
+  width: 100%;
+  align-items: center;
+`;
+
+export const StyledTokenRow = styled.div<{
+  first?: boolean;
+  last?: boolean;
+  loading?: boolean;
+}>`
+  background-color: transparent;
+  display: grid;
+  grid-template-columns: 2fr 4fr 2fr 2fr;
+  padding: 16px;
+  width: 100%;
+`;
